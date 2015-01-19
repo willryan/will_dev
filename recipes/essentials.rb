a3f1aed14c6014393a9e6ea762f15f84cd2bb2b6
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-01-19 08:50:53 -0500

    add working directory

--- recipes/essentials.rb
+++ recipes/essentials.rb
@@ -1,3 +1,16 @@
 package "git"
 package "libfontconfig1"
 package "libicu52"
+
+# working directory
+git "#{node["will_dev"]["home_path"]}/.wd" do
+  repository "https://github.com:karlin/working-directory.git"
+  action :checkout
+  user node["will_dev"]["username"]
+  group node["will_dev"]["username"]
+end
+
+execute "install.sh" do
+  cwd "#{node["will_dev"]["home_path"]}/.wd"
+  user node["wil_dev"]["username"]
+end
