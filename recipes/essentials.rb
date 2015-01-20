9caa1eb862cf6c13f5d0212d367f8851d08a44a2
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-01-20 16:50:10 -0500

    update wd install

--- recipes/essentials.rb
+++ recipes/essentials.rb
@@ -3,7 +3,7 @@ package "libfontconfig1"
 package "libicu52"
 
 # working directory
-git "#{node["will_dev"]["home_path"]}/.wd" do
+git "#{node["will_dev"]["home_path"]}/.working-directory-repo" do
   repository "git@github.com:karlin/working-directory.git"
   action :checkout
   user node["will_dev"]["username"]
@@ -11,6 +11,10 @@ git "#{node["will_dev"]["home_path"]}/.wd" do
 end
 
 execute "install.sh" do
-  cwd "#{node["will_dev"]["home_path"]}/.wd"
+  cwd "#{node["will_dev"]["home_path"]}/.working-directory-repo"
   user node["will_dev"]["username"]
+end
+
+directory "#{node["will_dev"]["home_path"]}/.working-directory-repo" do
+  action :delete
 end
