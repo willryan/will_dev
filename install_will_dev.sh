24b5600af24778c45eeb647db4d933a3d9013fb6
William Pleasant-Ryan <willryan@users.noreply.github.com>
2015-01-28 17:15:26 -0500

    don't install berkshelf, update source

--- install_will_dev.sh
+++ install_will_dev.sh
@@ -2,8 +2,7 @@
 
 mkdir install_will_dev
 cd install_will_dev
-gem install berkshelf
-echo "source 'https://supermarket.chef.com'\n\ncookbook 'will_dev', git: 'git://github.com/willryan/will_dev'\n" > Berksfile
+echo "source 'https://supermarket.chef.io'\n\ncookbook 'will_dev', git: 'git://github.com/willryan/will_dev'\n" > Berksfile
 berks vendor cookbooks
 chef-client -z -o will_dev
 cd ..
